10ce99c21179c4c93e7ca72618c9475f64c1398a
Nownitya Sharma <nownitya@gmail.com>
2024-09-06 17:36:46 +0530

    Starter_Main.

--- app/src/main/java/com/nowni/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/nowni/marsphotos/network/MarsApiService.kt
@@ -1,14 +1,9 @@
 package com.nowni.marsphotos.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.nowni.marsphotos.model.MarsPhoto
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 import retrofit2.http.GET
 
 interface MarsApiService {
     @GET("photos")
-    suspend fun getPhotos():List<MarsPhoto>
+    suspend fun getPhotos(): List<MarsPhoto>
 }
-
